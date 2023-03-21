cc55f355fae6799ab1748d2a785cb6ca4eea7d60
Gaurav Labhane <gaurav.labhane@vahan.co>
2023-03-22 01:01:21 +0530

    minor correction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const authorization = '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';
+const authorization = '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';
 const s = require("readline");
 const readline = s.createInterface({
     input: process.stdin,
@@ -17,13 +17,13 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const chatHiatory = [
     {"role": "system", "content": "You are a english teacher."},
-    {"role": "user", "content": "I am Pradip, a blue collar worker from india who knows hindi and I want to learn basic english "},
-    {"role": "assistant", "content": "Hello, Pradip. That's awesome! lrts start with some simple sentences"},
-    {"role": "user", "content": "Okay"}
+    {"role": "user", "content": "I am Pradip, a blue collar worker from india who knows hindi and I want to learn basic english.tell me some common sentences that i can use daily work and give their hindi explaination. "},
+    {"role": "assistant", "content": "Hello, Pradip. That's awesome! say start to start"}
 ]
 let ask = true;
  const question = async function(input = {}) {
     let query = input.question;
+    try {
     chatHiatory.push({"role": "user", "content": query})
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -35,6 +35,9 @@ let ask = true;
       ask = true;
       console.log(query, '\n', completion.data.choices[0].message.content);
       return true;
+    } catch (err) {
+        console.log(err);
+    }
  }
 
 // question({question: ''});
@@ -45,14 +48,7 @@ let ask = true;
         question({question: que});
         readline.close();
       });    
-      readline.question('enter: ', que => {
-        question({question: que});
-        readline.close();
-      });  
-      readline.question('enter: ', que => {
-        question({question: que});
-        readline.close();
-      });  
+
 // }
 
 
